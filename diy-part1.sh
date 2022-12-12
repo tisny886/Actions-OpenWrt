52f548a5f0a4ea55d5353b4614394551e5856966
tisny886 <71377748+tisny886@users.noreply.github.com>
2022-12-12 09:57:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,11 +9,12 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-
+#改内核5.10
 #sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.10/g' ./target/linux/ramips/Makefile
 #sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g' ./target/linux/ramips/Makefile
-
+#改内核5.15
 #sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.15/g' ./target/linux/ramips/Makefile
+
 # Uncomment a feed source
 
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
