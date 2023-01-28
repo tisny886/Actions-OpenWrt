21bd4cdd6bbaf9325a8c0bb858b0fb5d89bb58ac
tisny886 <71377748+tisny886@users.noreply.github.com>
2023-01-29 02:45:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -51,27 +51,27 @@ git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
 # 科学上网插件
 git clone --depth 1 https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
 git clone --depth 1 https://github.com/jerrykuku/lua-maxminddb package/lua-maxminddb
-svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
-svn co https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall package/luci-app-passwall
-svn co https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2 package/luci-app-passwall2
-svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
+svn co https://github.com/vernesong/OpenClash/luci-app-openclash package/luci-app-openclash
+#svn co https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall package/luci-app-passwall
+svn co https://github.com/xiaorouji/openwrt-passwall2/luci-app-passwall2 package/luci-app-passwall2
+svn co https://github.com/fw876/helloworld/luci-app-ssr-plus package/luci-app-ssr-plus
 
 # 科学上网插件依赖
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/brook package/brook
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/chinadns-ng package/chinadns-ng
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/dns2socks package/dns2socks
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/dns2tcp package/dns2tcp
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/hysteria package/hysteria
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/ipt2socks package/ipt2socks
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/microsocks package/microsocks
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/naiveproxy package/naiveproxy
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/pdnsd-alt package/pdnsd-alt
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/sagernet-core package/sagernet-core
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/ssocks package/ssocks
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/tcping package/tcping
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-go package/trojan-go
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-plus package/trojan-plus
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/v2ray-geodata package/v2ray-geodata
+svn co https://github.com/xiaorouji/openwrt-passwall/brook package/brook
+svn co https://github.com/xiaorouji/openwrt-passwall/chinadns-ng package/chinadns-ng
+svn co https://github.com/xiaorouji/openwrt-passwall/dns2socks package/dns2socks
+svn co https://github.com/xiaorouji/openwrt-passwall/dns2tcp package/dns2tcp
+svn co https://github.com/xiaorouji/openwrt-passwall/hysteria package/hysteria
+svn co https://github.com/xiaorouji/openwrt-passwall/ipt2socks package/ipt2socks
+svn co https://github.com/xiaorouji/openwrt-passwall/microsocks package/microsocks
+svn co https://github.com/xiaorouji/openwrt-passwall/naiveproxy package/naiveproxy
+svn co https://github.com/xiaorouji/openwrt-passwall/pdnsd-alt package/pdnsd-alt
+svn co https://github.com/xiaorouji/openwrt-passwall/sagernet-core package/sagernet-core
+svn co https://github.com/xiaorouji/openwrt-passwall/ssocks package/ssocks
+svn co https://github.com/xiaorouji/openwrt-passwall/tcping package/tcping
+svn co https://github.com/xiaorouji/openwrt-passwall/trojan-go package/trojan-go
+svn co https://github.com/xiaorouji/openwrt-passwall/trojan-plus package/trojan-plus
+svn co https://github.com/xiaorouji/openwrt-passwall/v2ray-geodata package/v2ray-geodata
 svn co https://github.com/fw876/helloworld/simple-obfs package/simple-obfs
 svn co https://github.com/fw876/helloworld/v2ray-core package/v2ray-core
 svn co https://github.com/fw876/helloworld/v2ray-plugin package/v2ray-plugin
