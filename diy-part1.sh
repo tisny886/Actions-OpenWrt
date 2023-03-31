79c36e26a8e33272e92db78fda718d85b5ee76ed
tisny886 <71377748+tisny886@users.noreply.github.com>
2023-04-01 01:59:32 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -83,5 +83,18 @@ svn co https://github.com/fw876/helloworld/trunk/lua-neturl package/lua-neturl
 svn co https://github.com/fw876/helloworld/trunk/trojan package/trojan
 svn co https://github.com/fw876/helloworld/trunk/redsocks2 package/redsocks2
 
-./scripts/feeds update -a
-./scripts/feeds install -a
+# Add luci-theme-opentomcat
+git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git theme-temp/luci-theme-opentomcat
+rm -rf theme-temp/luci-theme-opentomcat/LICENSE
+rm -rf theme-temp/luci-theme-opentomcat/README.md
+mv -f theme-temp/luci-theme-opentomcat package/lean/
+rm -rf theme-temp
+
+# Add luci-app-vssr
+git clone https://github.com/jerrykuku/lua-maxminddb.git package-temp/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr.git package-temp/luci-app-vssr
+git clone https://github.com/kenzok8/small.git package-temp/small
+cp -r package-temp/small/* package/lean/
+mv -f package-temp/lua-maxminddb package/lean/
+mv -f package-temp/luci-app-vssr package/lean/
+rm -rf package-temp
