1239aa313639eb0d3696be9db30cae8f3512da27
tisny886 <71377748+tisny886@users.noreply.github.com>
2026-09-09 03:15:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
- # 第三方插件源
- echo "src-git subconverter https://github.com/tindy2013/openwrt-subconverter.git;main" >> feeds.conf.default
- echo "src-git turboacc https://github.com/mufeng05/turboacc.git;main" >> feeds.conf.default
- echo "src-git openclash https://github.com/vernesong/OpenClash.git;master" >> feeds.conf.default
- echo "src-git mosdns https://github.com/sbwml/luci-app-mosdns.git;v5" >> feeds.conf.default
- echo "src-git easytier https://github.com/EasyTier/luci-app-easytier.git;main" >> feeds.conf.default
- echo "src-git zerotier https://github.com/mwarning/zerotier-openwrt.git;master" >> feeds.conf.default
- ./scripts/feeds update -a
- ./scripts/feeds install -a
+
+# 第三方插件源
+echo "src-git turboacc https://github.com/mufeng05/turboacc.git;main" >> feeds.conf.default
+echo "src-git openclash https://github.com/vernesong/OpenClash.git;master" >> feeds.conf.default
+echo "src-git mosdns https://github.com/sbwml/luci-app-mosdns.git;v5" >> feeds.conf.default
+echo "src-git easytier https://github.com/EasyTier/luci-app-easytier.git;main" >> feeds.conf.default
+echo "src-git zerotier https://github.com/mwarning/zerotier-openwrt.git;master" >> feeds.conf.default
+
+./scripts/feeds update -a
+./scripts/feeds install -a
