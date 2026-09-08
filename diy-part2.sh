7c1bee4369bd3c42eff63766a03e7c4de8f2db61
tisny886 <71377748+tisny886@users.noreply.github.com>
2026-09-09 02:45:17 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+ # 默认IP 192.168.6.1
+ sed -i 's/192.168.1.1/192.168.30.1/g' package/base-files/files/bin/config_generate
+ # 移除netifd firewall4依赖
+ sed -i 's/firewall4//g' package/network/config/netifd/Makefile
