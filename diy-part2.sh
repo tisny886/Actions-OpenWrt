c7c1f02d1b88d2a431f8a9447fb32b1465f41a6d
tisny886 <71377748+tisny886@users.noreply.github.com>
2026-09-09 03:16:29 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
- # 默认IP 192.168.6.1
- sed -i 's/192.168.1.1/192.168.30.1/g' package/base-files/files/bin/config_generate
- # 移除netifd firewall4依赖
- sed -i 's/firewall4//g' package/network/config/netifd/Makefile
+# 默认IP 192.168.6.1
+sed -i 's/192.168.1.1/192.168.30.1/g' package/base-files/files/bin/config_generate
+# 移除netifd firewall4依赖
+sed -i 's/firewall4//g' package/network/config/netifd/Makefile
